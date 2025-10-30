951c5874f6b1439e87190defb2295b46f07e570f
RolandoMamani <rolomc2016@gmail.com>
2025-10-30 17:36:45 -0500

    funcion editar usuario

--- src/componentes/ListarUsuario.jsx
+++ src/componentes/ListarUsuario.jsx
@@ -1,10 +1,24 @@
 import { useState, useEffect } from "react";
 import { db } from "../firebase";
-import { collection, onSnapshot } from "firebase/firestore";
+import { collection, onSnapshot, doc, updateDoc } from "firebase/firestore";
 
 function ListarUsuario() {
 
     const [usuarios, setUsuarios] = useState([]);
+
+    const [editandoID, setEditandoID] = useState(null);
+    const [nombreEditado, setNombreEditado] = useState("");
+    const [edadEditada, setEdadEditada] = useState("");
+
+    const editarUsuario = async (id) => {
+        const usuarioRef = doc(db, "usuarios", id);
+        await updateDoc(usuarioRef, {
+            nombre: nombreEditado,
+            edad:edadEditada
+        })
+        setEditandoID(null);
+    }
+
 
     //================================================
     // Funcion para obtener los usuarios
@@ -26,7 +40,7 @@ function ListarUsuario() {
             <h2>Lista de los usuarios</h2>
             <ul>
                 {usuarios.map((usuario)=>(
-                    <li key={usuario.id}>{usuario.nombre}</li>
+                    <li key={usuario.id}>{usuario.nombre} - {usuario.edad}</li>
                 ))}
                 
             </ul>
