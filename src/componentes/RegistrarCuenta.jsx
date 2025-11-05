fb3606090703deb58b376d2d8f25d66b8d661a0d
RolandoMamani <rolomc2016@gmail.com>
2025-11-05 18:25:42 -0500

    rgister

--- src/componentes/RegistrarCuenta.jsx
+++ src/componentes/RegistrarCuenta.jsx
@@ -1,10 +1,43 @@
+import { useState } from "react";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
 function RegistrarCuenta() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    //Funcionar para crear un usuario
+    const registrar = async() => {
+        const auth = getAuth();
+        createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed up 
+            const user = userCredential.user;
+            console.log("Usuario registrado");
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log("Error al crear tu cuenta");
+            console.log(error);
+        });
+    }
+
     return(
         <div>
             <h1>Registrar Cuenta</h1>
-            <input type="email" placeholder="correo" />
-            <input type="password" placeholder="contraseña"/>
-            <button>Registrar</button>
+            <input 
+                type="email" 
+                placeholder="correo" 
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}    
+            />
+            <input 
+                type="password" 
+                placeholder="contraseña"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}    
+            />
+            <button onClick={registrar}>Registrar</button>
         </div>
     )
 }
