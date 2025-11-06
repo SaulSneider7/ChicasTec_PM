303bb630e3166a9ea7f9d1978293d20ef14c89f5
RolandoMamani <rolomc2016@gmail.com>
2025-11-06 18:58:57 -0500

    login

--- src/componentes/Login.jsx
+++ src/componentes/Login.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup  } from "firebase/auth";
+const provider = new GoogleAuthProvider();
 
 
 function Login() {
@@ -7,10 +8,42 @@ function Login() {
     const [password, setPassword] = useState("");
 
     function iniciarSesion (){
-        console.log("cabello");
-        console.log(email);
-        console.log(password);
+        const auth = getAuth();
+        signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            // ...
+            console.log("inicio de sesion exitoso");
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log("Error al crear cuenta");
+            console.log(error);
+        });
     }
+
+    function iniciarConGoogle() {
+        const auth = getAuth();
+        signInWithPopup(auth, provider)
+        .then((result) => {
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+            const user = result.user;
+            // ...
+            console.log("Iniciaste sesion con Google");
+            
+        }).catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            const email = error.customData.email;
+            const credential = GoogleAuthProvider.credentialFromError(error);
+            // ...
+            console.log("Error con iniciar con Google");
+            console.log(error);            
+        });
+    }
+
 
     return(
         <div>
@@ -28,6 +61,7 @@ function Login() {
                 onChange={(e) => setPassword(e.target.value)}
             />
             <button onClick={iniciarSesion}>Iniciar Sesion</button>
+            <button onClick={iniciarConGoogle}>Ingresa con Google</button>
         </div>
     )
 }
